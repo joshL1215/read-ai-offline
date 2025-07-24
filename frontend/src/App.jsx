7fca994716b61c00f2a96d1d56d62b71cdab43f0
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-07-24 00:13:19 -0700

    Create mic visualization component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import MicVisualizer from './components/MicVisualizer.jsx'
 function App() {
+
+    return (
+        <MicVisualizer />
+    );
 }
+
 
 export default App;
---
+++ frontend/src/components/MicVisualizer.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import MicIcon from '@mui/icons-material/Mic';
+import { Avatar, Box, Typography } from "@mui/material";
+
+function MicVisualizer() {
+    const [voiceDetected, setVoiceDetected] = useState(false);
+
+    useEffect(() => {
+
+        const socket = new WebSocket("ws://localhost:8000/ws/transcribe");
+        let animationId;
+
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+
+                const audioContext = new AudioContext();
+                const source = audioContext.createMediaStreamSource(stream);
+                const analyser = audioContext.createAnalyser();
+                analyser.fftSize = 256;
+
+                const dataArray = new Uint8Array(analyser.frequencyBinCount);
+                source.connect(analyser);
+
+                function getVolume() {
+
+                    analyser.getByteFrequencyData(dataArray);
+                    const sum = dataArray.reduce((acc, val) => acc + val, 0);
+                    const average = sum / dataArray.length;
+
+                    setVoiceDetected(average > 30);
+                    animationId = requestAnimationFrame(getVolume);
+
+                }
+
+                getVolume();
+
+            })
+
+            .catch(err => {
+                console.error("Mic not found", err);
+            });
+
+        return () => {
+            cancelAnimationFrame(animationId);
+        };
+    }, []);
+
+
+    return (
+        <Box
+            sx={{
+
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                mt: 4,
+                gap: 5,
+            }}
+        >
+            <Avatar
+                sx={{
+                    bgcolor: voiceDetected ? "#79cda2d4" : "grey.500",
+                    width: 80,
+                    height: 80,
+                    transition: "all 0.25s ease",
+                }}
+            >
+                <MicIcon sx={{ fontSize: voiceDetected ? 45 : 40 }} />
+            </Avatar>
+
+            <Typography
+                sx={{
+                    fontSize: 20,
+                }}
+            >
+                Live Transcription:
+            </Typography>
+        </Box>
+    );
+}
+
+export default MicVisualizer;
