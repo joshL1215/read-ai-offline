836b54ea9fc0f39afc877ea2b2131e5c693485b5
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-07-28 15:30:13 -0700

    Create start menu ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { CssBaseline } from '@mui/material';
-
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import MenuScene from './scenes/MenuScene.jsx';
 import ReadingScene from './scenes/ReadingScene.jsx';
 import JournalScene from './scenes/JournalScene.jsx';
+import '@fontsource/inter/300.css';
 
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Inter, Helvetica, sans-serif',
+    },
+});
 
 function App() {
-
     return (
-        <>
+        <ThemeProvider theme={theme} >
             <CssBaseline />
             <Routes>
                 <Route path="/" element={<MenuScene />} />
                 <Route path="/reading" element={<ReadingScene />} />
                 <Route path="/journal" element={<JournalScene />} />
             </Routes>
-
-        </>
-
-
-    )
-
+        </ThemeProvider>
+    );
 }
 
 export default App;
